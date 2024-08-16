f26843b79102a5d12b544f9b7e986c2f0e7e5d7e
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-16 18:53:36 +0900

    a header

---
+++ CMTime+/Sources/CMTime+.h
@@ -0,0 +1,13 @@
+//
+//  CMTime+.h
+//  
+//
+//  Created by kotan.kn on 8/10/R6.
+//
+#ifndef CMTime__h
+#define CMTime__h
+#include<CoreMedia/CoreMedia.h>
+CMTime const CMTimeMul(CMTime const, CMTime const);
+CMTime const CMTimeDiv(CMTime const, CMTime const);
+CMTime const CMTimeMod(CMTime const, CMTime const);
+#endif /* CMTime__h */
