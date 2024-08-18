b0a61316caddee56fa05248607038e817ef23181
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-18 20:05:58 +0900

    update

--- OSC/Snippets/Example.swift
+++ OSC/Snippets/Example.swift
@@ -9,21 +9,45 @@ import Network
 import SwiftUI
 import OSC
 final class Receiver: ObservableObject {
-	var cancellable: Optional<AnyCancellable> = .none
+	var dispatcher: Dispatcher<IPv4Endpoint> = .init()
 	@Published var description: String = ""
 	init(port: UInt16) {
-		cancellable = OSC.UdpReceiver(on: IPv4Endpoint(addr: .loopback, port: .init(integerLiteral: port))).sink(receiveCompletion: { complete in
-			
-		}, receiveValue: { (endpoint, message, options) in
-			DispatchQueue.main.sync { [weak self] in
-				guard let self else { return }
-				description = [message, options.map(String.init).joined(separator: ", "), "from", endpoint.description].joined(separator: " ")
-				objectWillChange.send()
-			}
-		})
-	}
-	deinit {
-		cancellable?.cancel()
+		// accept any message
+		dispatcher.add(for: "/address") { [weak self] address, arguments, endpoint in
+			guard let self else { return }
+			description = [address, arguments.description, "from", endpoint.description].joined(separator: " ")
+			objectWillChange.send()
+		}
+		/*
+		 Single dispatcher can route multiple address
+		dispatcher.add(for: "/address/sub1") { [weak self] address, arguments, endpoint in
+			guard let self else { return }
+			description = [address, "from", endpoint.description].joined(separator: " ")
+			objectWillChange.send()
+		}
+		 Entry dispatcher with regex literal, RegexBuilder object and OSC pattern matcher
+		dispatcher.add(for: /address/sub?/) { [weak self] address, arguments, endpoint in
+			guard let self else { return }
+			description = [address, "from", endpoint.description].joined(separator: " ")
+			objectWillChange.send()
+		}
+		dispatcher.add(for: try!Regex(osc: "/root/sub?/{foo,bar}")) { [weak self] address, arguments, endpoint in
+			guard let self else { return }
+			description = [address.compactMap { $0.substring }.joined(), "from", endpoint.description].joined(separator: " ")
+			objectWillChange.send()
+		}
+		 */
+		// subscribe publisher
+		OSC.UdpReceiver(on: IPv4Endpoint(addr: .loopback, port: .init(integerLiteral: port)))
+			.receive(on: RunLoop.main)
+			.receive(subscriber: dispatcher)
+		/*
+		 Single dispatcher can accept multiple receiver
+		OSC.UdpReceiver(on: IPv4Endpoint(addr: .loopback, port: .init(integerLiteral: 16384)))
+			.receive(on: RunLoop.main)
+			.receive(subscriber: dispatcher)
+		 */
+		
 	}
 }
 @main
@@ -65,11 +89,19 @@ struct SenderView: View {
 	let sender = OSC.UdpSender<IPv4Endpoint>()
 	@State var host: String = "127.0.0.1"
 	@State var port: UInt16 = 16384
-	@State var message: String = "/address"
-	@State var argument: Int32 = 0
+	@State var address: String = "/address"
+	@State var argument: Int64 = 1
 	func send() {
 		guard let target = IPv4Address(host) else { return }
-		sender.send(message: message, with: [.init(argument)], to: .init(addr: target, port: NWEndpoint.Port(integerLiteral: port)))
+		var message = Message(address)
+		message.append(argument)
+		/*
+		 A message can have more arguments
+		 message.append(1.0 as Float32)
+		 message.append("string")
+		 message.append(Data())
+		 */
+		sender.send(message: message, to: .init(addr: target, port: NWEndpoint.Port(integerLiteral: port)))
 	}
 	var body: some View {
 		VStack {
@@ -78,7 +110,7 @@ struct SenderView: View {
 				TextField("Port", value: $port, format: .number)
 			}
 			HStack {
-				TextField("Address", text: $message)
+				TextField("Address", text: $address)
 				TextField("Argument", value: $argument, format: .number)
 			}
 			// Push button to send message and integer argument to remote osc receiver
