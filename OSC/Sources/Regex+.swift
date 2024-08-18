b35f76e4bfe0bf60c9fc3db76997c63d3ab01da3
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-18 19:50:26 +0900

    add glob -> regex translater for osc address pattern match

--- OSC/Sources/Regex+.swift
+++ OSC/Sources/Regex+.swift
@@ -5,57 +5,112 @@
 //  Created by Kota on 8/17/R6.
 //
 import RegexBuilder
+import Darwin
+@usableFromInline
+enum Context {
+	case none
+	case quote
+	case brace
+	case brack
+}
+@inlinable
+func translate(osc fnmatch: inout Substring, context: Context) -> String {
+	switch fnmatch.popFirst() {
+	case.some("\\") where context != .quote:
+		return "\\" + (fnmatch.popFirst().map(String.init) ?? "")
+	case.some("^") where context != .quote:
+		return "\\^"
+	case.some("$") where context != .quote:
+		return "\\$"
+	case.some(".") where context != .quote:
+		return "\\."
+	case.some("+") where context != .quote:
+		return "\\+"
+	case.some("*") where context != .quote && fnmatch.first == .some("*"):
+		defer {
+			fnmatch.removeFirst()
+		}
+		return ".*"
+	case.some("*") where context != .quote:
+		return "[^/]*"
+	case.some("?") where context != .quote:
+		return "[^/]"
+	case.some("\"") where context != .quote:
+		var result = ""
+		repeat {
+			switch fnmatch.first {
+			case.some("\""):
+				defer {
+					fnmatch.removeFirst()
+				}
+				return result
+			case.none:
+				return "\\\"" + result
+			default:
+				result.append(translate(osc: &fnmatch, context: .quote))
+			}
+		} while true
+	case.some("\""):
+		return "\\\""
+	case.some("!") where context == .brack:
+		return "^"
+	case.some("-") where context != .brack:
+		return "\\-"
+	case.some("[") where context != .quote:
+		var result = ""
+		repeat {
+			switch fnmatch.first {
+			case.some("]"):
+				defer {
+					fnmatch.removeFirst()
+				}
+				return "[" + result + "]"
+			case.none:
+				return "\\[" + result
+			default:
+				result.append(translate(osc: &fnmatch, context: .brack))
+			}
+		} while true
+	case.some("]") where context != .quote:
+		return "\\]"
+	case.some(",") where context == .brace:
+		return "|"
+	case.some("{") where context != .quote:
+		var result = ""
+		repeat {
+			switch fnmatch.first {
+			case.some("}"):
+				defer {
+					fnmatch.removeFirst()
+				}
+				return "(?s:" + result + ")"
+			case.none:
+				return "\\{" + result
+			default:
+				result.append(translate(osc: &fnmatch, context: .brace))
+			}
+		} while true
+	case.some("}") where context != .quote:
+		return "\\}"
+	case.some("("):
+		return "\\("
+	case.some(")"):
+		return "\\)"
+	case.some(let value):
+		return.init(value)
+	case.none:
+		return ""
+	}
+}
+@inlinable // like fnmatch.translate
+func translate(osc fnmatch: some StringProtocol) -> String {
+	"^" + sequence(state: Substring(fnmatch)) {
+		$0.isEmpty ? .none : .some(translate(osc: &$0, context: .none))
+	}.joined() + "$"
+}
 extension Regex where RegexOutput == AnyRegexOutput {
 	@inlinable
-	public init(glob pattern: some StringProtocol) throws {
-		try self.init(("^" + sequence(state: pattern.reversed()) {
-			while let char = $0.popLast() {
-				switch char {
-				case "?":
-					return.some("[^/]?" as String)
-				case "*" where $0.last == .some("*"):
-					return.some(".*")
-				case "*":
-					return.some("[^/]*")
-				case "{":
-					var segment = ""
-					var escape = false
-					repeat {
-						switch $0.popLast() {
-						case.some("\""):
-							escape.toggle()
-						case.some("}"):
-							return.some("(" + segment + ")")
-						case.some(",") where !escape:
-							segment.append("|")
-						case.some(let char):
-							segment.append(char)
-						case.none:
-							return.some(segment)
-						}
-					} while !$0.isEmpty
-				case "[":
-					var segment = ""
-					var escape = false
-					repeat {
-						switch $0.popLast() {
-						case.some("\""):
-							escape.toggle()
-						case.some("]"):
-							return.some("[" + segment + "]")
-						case.some("!") where !escape:
-							segment.append("^")
-						case.some(let char):
-							segment.append(char)
-						case.none:
-							return.some(segment)
-						}
-					} while !$0.isEmpty
-				case let value:
-					return.some(String(value))
-				}
-			}
-			return.none
-		}.joined() + "$"))
+	public init(osc pattern: some StringProtocol) throws {
+		try self.init(translate(osc: pattern))
 	}
 }
