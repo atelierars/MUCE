fbb2af8090a0cbde1e114b9f322bd5bf46d8ddb7
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-18 19:52:49 +0900

    apply message type

--- OSC/Sources/Receiver.swift
+++ OSC/Sources/Receiver.swift
@@ -9,6 +9,7 @@ import Socket
 import Dispatch
 import Foundation
 import Network
+import os.log
 @_exported import struct Socket.IPv4Endpoint
 @_exported import struct Socket.IPv6Endpoint
 //public func UdpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Optional<DispatchQueue> = .none) -> AsyncCompactMapSequence<AsyncThrowingStream<(Data, Endpoint), Error>, (Packet, Endpoint)> {
@@ -16,13 +17,13 @@ import Network
 //		Packet(rawValue: data).map { ($0, endpoint) }
 //	}
 //}
-public func UdpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Optional<DispatchQueue> = .none) -> some Publisher<(Endpoint, String, Array<Argument>), NWError> {
+public func UdpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Optional<DispatchQueue> = .none) -> some Publisher<(Message, Endpoint), NWError> {
 	UdpStream.Incoming(on: endpoint, queue: queue)
 		.publisher
 		.flatMap { $0 }
 		.compactMap { (data, endpoint) in Packet(rawValue: data).map { ($0, endpoint) } }
 		.flatMap { (packet, endpoint) in
-			packet.publisher.map { (endpoint, $0, $1) }
+			packet.publisher.map { ($0, endpoint) }
 		}
 }
 //public func UdpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Optional<DispatchQueue> = .none) -> some Publisher<(Packet, Endpoint), NWError> {
@@ -36,10 +37,10 @@ public func UdpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Opti
 //		stream.compactMap(Packet.init(rawValue:)).map { ($0, endpoint) }
 //	}
 //}
-public func TcpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Optional<DispatchQueue> = .none, count: Int = 1) -> some Publisher<(Endpoint, String, Array<Argument>), NWError> {
+public func TcpReceiver<Endpoint: IPEndpoint>(on endpoint: Endpoint, queue: Optional<DispatchQueue> = .none, count: Int = 1) -> some Publisher<(Message, Endpoint), NWError> {
 	TcpStream.Incoming(on: endpoint, count: count, queue: queue)
 		.flatMap { (stream, endpoint) in stream.compactMap { Packet(rawValue: $0).map { ($0, endpoint) } } }
 		.flatMap { (packet, endpoint) in
-			packet.publisher.map { (endpoint, $0, $1) }
+			packet.publisher.map { ($0, endpoint) }
 		}
 }
