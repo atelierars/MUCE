70ac1c80370033f18a02ac898e9cb0e54a0f6511
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-19 15:47:22 +0900

    employ new extensional Argument

--- OSC/Sources/Message.swift
+++ OSC/Sources/Message.swift
@@ -2,7 +2,7 @@
 //  Message.swift
 //
 //
-//  Created by Kota on 8/17/R6.
+//  Created by kotan.kn on 8/17/R6.
 //
 import protocol Foundation.DataProtocol
 import struct Foundation.Data
@@ -10,7 +10,7 @@ import RegexBuilder
 @frozen public struct Message {
 	public let address: String
 	@usableFromInline
-	var arguments: Array<Argument>
+	var arguments: Arguments
 	public init(_ address: some StringProtocol, with arguments: some Sequence<Argument> = []) {
 		self.address = address.replacingOccurrences(of: "\0", with: "")
 		self.arguments = .init(arguments)
@@ -22,58 +22,9 @@ extension Message: ExpressibleByStringLiteral {
 	}
 }
 extension Message {
-	// standard
 	@inlinable
-	public mutating func append(_ value: Int32) {
-		arguments.append(.i32(value))
-	}
-	@inlinable
-	public mutating func append(_ value: Float32) {
-		arguments.append(.f32(value))
-	}
-	@inlinable
-	public mutating func append(_ value: some StringProtocol) {
-		arguments.append(.text(.init(value)))
-	}
-	@inlinable
-	public mutating func append(_ value: some DataProtocol) {
-		arguments.append(.blob(.init(value)))
-	}
-	// alternative
-	@inlinable
-	public mutating func append(_ value: Int64) {
-		arguments.append(.i64(value))
-	}
-	@inlinable
-	public mutating func append(_ value: Float64) {
-		arguments.append(.f64(value))
-	}
-	@inlinable
-	public mutating func append(_ value: Character) {
-		arguments.append(.char(value))
-	}
-	@inlinable
-	public mutating func append(_ value: TimeTag) {
-		arguments.append(.time(value))
-	}
-	// special
-	@inlinable
-	public mutating func append(_ value: some Sequence<Argument>) {
-		arguments.append(.array(.init(value)))
-	}
-	@inlinable
-	public mutating func append(_ value: Bool) {
-		arguments.append(.bool(value))
-	}
-	@inlinable
-	public mutating func append(_ value: ()) {
-		arguments.append(.nil)
-	}
-}
-extension Message {
-	@inlinable
-	public var isStandard: Bool {
-		address.starts(with: "/") && arguments.allSatisfy { $0.isStandard }
+	public mutating func append(_ value: some Argument) {
+		arguments.append(value)
 	}
 }
 extension Message: RandomAccessCollection & MutableCollection {
@@ -107,7 +58,6 @@ extension Message {
 		rhs.address.wholeMatch(of: lhs) != nil
 	}
 }
-extension Message: Equatable {}
 extension Message: CustomStringConvertible {
 	@inlinable
 	public var description: String {
