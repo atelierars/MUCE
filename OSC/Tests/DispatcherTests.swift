72e2bf00781543cf02f26494908ad78cf90d3140
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-19 15:50:38 +0900

    add pattern match based tests

--- OSC/Tests/DispatcherTests.swift
+++ OSC/Tests/DispatcherTests.swift
@@ -2,33 +2,19 @@ import XCTest
 @testable import OSC
 final class RouterTests: XCTestCase {
 	func testScenario() {
-		let helloworld = XCTestExpectation(description: "hello world")
-		helloworld.expectedFulfillmentCount = 2
-		let send = XCTestExpectation(description: "send")
-//		let recv = XCTestExpectation(description: "recv")
-//		var vtable = Router()
-//		vtable.append(for: "/hello/world") {
-//			XCTAssertTrue($1.isEmpty)
-//			helloworld.fulfill()
-//		}
-//		vtable.append(for: /^\/hello\/world$/) {
-//			XCTAssertTrue($1.isEmpty)
-//			helloworld.fulfill()
-//		}
-//		vtable.append(for: /^\/(send)\/hello\/([a-z]+)/) {
-//			XCTAssertEqual($0.1, "send")
-//			XCTAssertEqual($0.2, "value")
-//			XCTAssertEqual($1.dropFirst(0).first, 1)
-//			XCTAssertEqual($1.dropFirst(1).first, 2.0)
-//			XCTAssertEqual($1.dropFirst(2).first, "test")
-//			send.fulfill()
-//		}
-//		vtable.append(for: /^\/(recv)\/hello\/([a-z]+)/) {(k, v)in
-//			XCTFail()
-//		}
-//		vtable.dispatch(message: .init(address: "/hello/world", arguments: []))
-//		vtable.dispatch(message: .init(address: "/send/hello/value", arguments: [1, 2.0, "test"]))
-////		vtable.dispatch(message: .init(address: "/recv/hello/value", arguments: []))
-//		wait(for: [helloworld, send], timeout: 0)
+		let expectation = XCTestExpectation()
+		expectation.expectedFulfillmentCount = 4
+		let dispatcher = Dispatcher<IPv4Endpoint>()
+		dispatcher.add(for: /root\/sub?/) { _, _, _ in
+			expectation.fulfill()
+		}
+		dispatcher.add(for: /root\/sub?/) { _, _, _ in
+			expectation.fulfill()
+		}
+		dispatcher.receive(("/root/sub1", IPv4Endpoint(addr: .any, port: .any)))
+		dispatcher.receive(("/root/sub2", IPv4Endpoint(addr: .any, port: .any)))
+		dispatcher.receive(("/root/sub3", IPv4Endpoint(addr: .any, port: .any)))
+		dispatcher.receive(("/root/sub4", IPv4Endpoint(addr: .any, port: .any)))
+		wait(for: [expectation], timeout: 1)
 	}
 }
