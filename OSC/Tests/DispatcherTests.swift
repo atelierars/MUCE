57b71c054d3aee6a165dc08eb50d457ffe4940e1
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-19 20:24:33 +0900

    update tests

--- OSC/Tests/DispatcherTests.swift
+++ OSC/Tests/DispatcherTests.swift
@@ -1,20 +1,47 @@
 import XCTest
 @testable import OSC
 final class RouterTests: XCTestCase {
-	func testScenario() {
+	func testServerRegex() throws {
 		let expectation = XCTestExpectation()
-		expectation.expectedFulfillmentCount = 4
+		expectation.expectedFulfillmentCount = 8
 		let dispatcher = Dispatcher<IPv4Endpoint>()
-		dispatcher.add(for: /root\/sub?/) { _, _, _ in
+		// accept multiple address, we can capture the elements by using regex
+		dispatcher.invoke(for: /^\/root\/sub(?<number>.)$/) { address, _, _ in
+			// evaluate captured values
+			XCTAssertTrue(address.number.allSatisfy { $0.isNumber })
 			expectation.fulfill()
 		}
-		dispatcher.add(for: /root\/sub?/) { _, _, _ in
+		dispatcher.invoke(for: /^\/root\/sud.$/) { _, _, _ in
+			XCTFail()
+		}
+		// we can use glob style, same as osc
+		dispatcher.invoke(for: try Regex(osc: "/root/sub?")) { _, _, _ in
 			expectation.fulfill()
 		}
 		dispatcher.receive(("/root/sub1", IPv4Endpoint(addr: .any, port: .any)))
 		dispatcher.receive(("/root/sub2", IPv4Endpoint(addr: .any, port: .any)))
 		dispatcher.receive(("/root/sub3", IPv4Endpoint(addr: .any, port: .any)))
 		dispatcher.receive(("/root/sub4", IPv4Endpoint(addr: .any, port: .any)))
-		wait(for: [expectation], timeout: 1)
+		wait(for: [expectation], timeout: 0)
+	}
+	// In this scenario, a message will be accepted by multiple handlers
+	// a message /root/sub?/{foo,bar} will be accepted by multiple handlers /root/sub1/foo, /root/sub2/bar
+	func testClientQuery() throws {
+		let expectation = XCTestExpectation()
+		expectation.expectedFulfillmentCount = 2
+		let dispatcher = Dispatcher<IPv4Endpoint>()
+		dispatcher.invoke(for: "/root/sub1/foo") { _, _, _ in
+			expectation.fulfill()
+		}
+		dispatcher.invoke(for: "/root/sub2/bar") { _, _, _ in
+			expectation.fulfill()
+		}
+		// not recept
+		dispatcher.invoke(for: "/root/sub3/foobar") { _, _, _ in
+			XCTFail()
+		}
+		// call multiple resources like query, by using glob form
+		dispatcher.receive(("/root/sub?/{foo,bar}", IPv4Endpoint(addr: .any, port: .any)))
+		wait(for: [expectation], timeout: 0)
 	}
 }
