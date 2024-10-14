88a2a5b50992223317d27c03289686cf2c31df59
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-14 19:17:00 +0900

    update async wrapper

--- Async+/Sources/DispatchQueue+.swift
+++ Async+/Sources/DispatchQueue+.swift
@@ -1,25 +1,25 @@
 //
 //  DispatchQueue+.swift
-//  
+//  MUCE
 //
 //  Created by kotan.kn on 8/3/R6.
 //
 import class Dispatch.DispatchQueue
 extension DispatchQueue {
 	@inlinable
-	public func async<R: Sendable>(execute body: @Sendable @escaping () -> R) async -> R {
+	public func async<R: Sendable>(execute body: @Sendable @escaping () -> sending R) async -> sending R {
 		await withUnsafeContinuation { promise in
 			async { promise.resume(with: .success(body())) }
 		}
 	}
 	@inlinable
-	public func async<Success>(execute body: @Sendable @escaping () -> Result<Success, Never>) async -> Success {
+	public func async<Success>(execute body: @Sendable @escaping () -> sending Result<Success, Never>) async -> sending Success {
 		await withUnsafeContinuation { promise in
 			async { promise.resume(with: body()) }
 		}
 	}
 	@inlinable
-	public func async<Success, Failure>(execute body: @Sendable @escaping () -> Result<Success, Failure>) async throws -> Success {
+	public func async<Success, Failure>(execute body: @Sendable @escaping () -> sending Result<Success, Failure>) async throws -> sending Success {
 		try await withUnsafeThrowingContinuation { promise in
 			async { promise.resume(with: body()) }
 		}
