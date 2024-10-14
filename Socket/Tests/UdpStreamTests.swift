b8bfaee85bba9d659437fc3ee375fb01fe1e6a54
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-14 19:41:30 +0900

    use Swift-Testing

--- Socket/Tests/UdpStreamTests.swift
+++ Socket/Tests/UdpStreamTests.swift
@@ -1,6 +1,9 @@
-import XCTest
+import Testing
+import Synchronization
+import Foundation
 @testable import Socket
-final class UdpStreamTests: XCTestCase {
+@Suite
+struct UdpStreamTests {
 	let req = Data(Array<UInt8>(unsafeUninitializedCapacity: 32) {
 		arc4random_buf($0.baseAddress, $0.count)
 		$1 = $0.count
@@ -10,31 +13,42 @@ final class UdpStreamTests: XCTestCase {
 		$1 = $0.count
 	})
 	func scenario<Endpoint: IPEndpoint>(for endpoint: Endpoint) async throws {
-		let request = XCTestExpectation(description: "request")
-		let response = XCTestExpectation(description: "response")
-		let server = try UdpStream.Incoming(on: endpoint).get()
-		Task {
-			for try await (data, endpoint) in server.values.prefix(1) where data == req {
-				request.fulfill()
-				let sent = try server.send(data: res, to: endpoint).get()
-				XCTAssertEqual(sent, res.count)
+		try await withThrowingDiscardingTaskGroup { group in
+			let server = try UdpStream.Incoming(on: endpoint).get()
+			group.addTask {
+				let status = Atomic(false)
+				for try await (data, endpoint) in server.values.prefix(1) {
+					#expect(data == req)
+					let sent = try server.send(data: res, to: endpoint).get()
+					#expect(sent == res.count)
+					status.store(true, ordering: .releasing)
+				}
+				let done = status.load(ordering: .acquiring)
+				#expect(done)
 			}
-		}
-		try await Task.sleep(for: Duration.milliseconds(1))
-		let client = try UdpStream<Endpoint>.Any().get()
-		let sent = try client.send(data: req, to: endpoint).get()
-		Task {
-			for try await (data, endpoint) in client.values.prefix(1) where data == res {
-				XCTAssertEqual(data, res)
-				response.fulfill()
+			try await Task.sleep(for: Duration.milliseconds(42))
+			let client = try UdpStream<Endpoint>.Any().get()
+			group.addTask {
+				let status = Atomic(false)
+				for try await (data, receiver) in client.values.prefix(1) {
+					#expect(data == res)
+					#expect(endpoint.addr == receiver.addr)
+					#expect(endpoint.port == receiver.port)
+					status.store(true, ordering: .releasing)
+				}
+				let done = status.load(ordering: .acquiring)
+				#expect(done)
 			}
+			let sent = try client.send(data: req, to: endpoint).get()
+			#expect(sent == req.count)
+			#expect(!group.isEmpty)
 		}
-		XCTAssertEqual(sent, req.count)
-		await fulfillment(of: [request, response], timeout: 6)
 	}
+	@Test(TimeLimitTrait.timeLimit(.minutes(1)))
 	func testScenarioV4() async throws {
 		try await scenario(for: IPv4Endpoint(addr: .loopback, port: 2048))
 	}
+	@Test(TimeLimitTrait.timeLimit(.minutes(1)))
 	func testScenarioV6() async throws {
 		try await scenario(for: IPv6Endpoint(addr: .loopback, port: 2049))
 	}
