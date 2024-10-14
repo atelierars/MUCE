af6701947264742836248213d17b0e790abfc632
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-14 19:42:01 +0900

    use Swift-Testing

--- Socket/Tests/IPEndpointTests.swift
+++ Socket/Tests/IPEndpointTests.swift
@@ -1,14 +1,12 @@
-import XCTest
+import Testing
 import Network
-import Combine
 @testable import Socket
-final class IPEndpointTestCases: XCTestCase {
-	func testIPv4Endpoint() {
-		let origin = IPv4Address("1.2.3.4").unsafelyUnwrapped
-		let eval = IPv4Endpoint(addr: origin, port: .http)
-		XCTAssertEqual(eval.addr, origin)
-		XCTAssertEqual(eval.port, 80)
-		XCTAssertEqual(UInt32(bigEndian: eval.sin_addr.s_addr), 0x01020304)
-		XCTAssertEqual(UInt16(bigEndian: eval.sin_port), 80)
-	}
+@Test
+func ipv4Endpoint() {
+	let origin = IPv4Address("1.2.3.4").unsafelyUnwrapped
+	let eval = IPv4Endpoint(addr: origin, port: .http)
+	#expect(eval.addr == origin)
+	#expect(eval.port == 80)
+	#expect(UInt32(bigEndian: eval.sin_addr.s_addr) == 0x01020304)
+	#expect(UInt16(bigEndian: eval.sin_port) == 80)
 }
