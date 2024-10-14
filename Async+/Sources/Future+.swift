ab84a55f84904cd154a18e515063e75b8c0e2150
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-14 19:17:22 +0900

    apply typecast for Swift 6.0

--- Async+/Sources/Future+.swift
+++ Async+/Sources/Future+.swift
@@ -1,12 +1,17 @@
 //
 //  Future+.swift
-//
+//  MUCE
 //
 //  Created by kotan.kn on 8/4/R6.
 //
 import class Combine.Future
 extension Future {
-	public convenience init(_ attemptToFulfill: @escaping ((Result<Output, Failure>) -> Void) async -> Void) {
+	public convenience init(_ attemptToFulfill: @escaping(@escaping @Sendable(Result<Output, Failure>) -> Void) -> Void) {
+		self.init { promise in
+			attemptToFulfill(unsafeBitCast(promise, to: (@Sendable(Result<Output, Failure>) -> Void).self))
+		}
+	}
+	public convenience init(_ attemptToFulfill: @escaping @Sendable(@Sendable(Result<Output, Failure>) -> Void) async -> Void) {
 		self.init { promise in
 			Task {
 				await attemptToFulfill(promise)
