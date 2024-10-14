dcdec61f077db0043cb0c19d74d5b0cc2cbb4aab
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-14 19:41:01 +0900

    employ Swift-Testing instead of XCTests

--- Socket/Tests/UdpSocketTests.swift
+++ Socket/Tests/UdpSocketTests.swift
@@ -1,7 +1,10 @@
-import XCTest
+import Testing
 import Combine
+import Foundation
+import Async_
 @testable import Socket
-final class UdpSocketTests: XCTestCase {
+@Suite
+struct UdpSocketTests {
 	let req = Data(Array<UInt8>(unsafeUninitializedCapacity: 32) {
 		arc4random_buf($0.baseAddress, $0.count)
 		$1 = $0.count
@@ -10,32 +13,37 @@ final class UdpSocketTests: XCTestCase {
 		arc4random_buf($0.baseAddress, $0.count)
 		$1 = $0.count
 	})
-	func scenario<Endpoint: IPEndpoint>(for endpoint: Endpoint) throws {
-		let request = XCTestExpectation(description: "request")
-		let response = XCTestExpectation(description: "response")
-		let server = try UdpSocket<Endpoint>.new.get()
-		try server.set(reuseAddr: true).get()
-		try server.set(reusePort: true).get()
-		try server.bind(on: endpoint).get()
-		Task {
-			let(packet, client) = try server.recv(count: 1024).get()
-			XCTAssertEqual(packet, req)
-			let sent = server.send(data: res, to: client)
-			XCTAssertEqual(sent, .success(res.count))
-			request.fulfill()
+	func scenario<Endpoint: IPEndpoint>(for endpoint: Endpoint) async throws {
+		try await withThrowingDiscardingTaskGroup { group in
+			let server = try UdpSocket<Endpoint>.new.get()
+			try server.set(reuseAddr: true).get()
+			try server.set(reusePort: true).get()
+			try server.bind(on: endpoint).get()
+			#expect(group.isEmpty)
+			group.addTask {
+				let (packet, client) = try server.recv(count: 1024).get()
+				#expect(packet == req)
+				let sent = server.send(data: res, to: client)
+				#expect(sent == .success(res.count))
+			}
+			try await Task.sleep(for: .milliseconds(42))
+			group.addTask {
+				let client = try UdpSocket<Endpoint>.new.get()
+				let sent = client.send(data: req, to: endpoint)
+				#expect(sent == .success(req.count))
+				let (packet, target) = try client.recv(count: 1024).get()
+				#expect(packet == res)
+				#expect(target == endpoint)
+			}
+			#expect(!group.isEmpty)
 		}
-		let client = try UdpSocket<Endpoint>.new.get()
-		let sent = client.send(data: req, to: endpoint)
-		XCTAssertEqual(sent, .success(req.count))
-		let(packet, _) = try client.recv(count: 1024).get()
-		XCTAssertEqual(packet, res)
-		response.fulfill()
-		wait(for: [request, response], timeout: 30)
 	}
-	func testScenarioV4() throws {
-		try scenario(for: IPv4Endpoint(addr: .loopback, port: 2048))
+	@Test(TimeLimitTrait.timeLimit(.minutes(1)))
+	func v4() async throws {
+		try await scenario(for: IPv4Endpoint(addr: .loopback, port: 2048))
 	}
-	func testScenarioV6() throws {
-		try scenario(for: IPv6Endpoint(addr: .loopback, port: 2049))
+	@Test(TimeLimitTrait.timeLimit(.minutes(1)))
+	func v6() async throws {
+		try await scenario(for: IPv6Endpoint(addr: .loopback, port: 2049))
 	}
 }
