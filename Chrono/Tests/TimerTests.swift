cd36b216e323ea401ff55b784ab2bb37a941b667
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-15 02:30:49 +0900

    use Swift Testing, employ quantiser

--- Chrono/Tests/TimerTests.swift
+++ Chrono/Tests/TimerTests.swift
@@ -1,28 +1,31 @@
-import XCTest
+import Testing
 import CoreMedia
+import Synchronization
+import Integer_
 @testable import Chrono
-final class TimerTests: XCTestCase {
-//	func testInteval() async throws {
-//		let ticker = try Ticker()
-//		try ticker.set(time: .init(value: -9000, timescale: 30000))
-//		try ticker.set(rate: -1)
-//		let timer = ticker.timer(for: .init(value: 1001, timescale: 30000))
-//		for try await time in timer.values.prefix(20) {
-//			print(time.convertScale(30000, method: .default), ticker.time.convertScale(30000, method: .default))
-//		}
-//	}
-	func testOffset() throws {
-		let expectation = XCTestExpectation()
+@Suite
+struct TimerTests {
+	@Test(TimeLimitTrait.timeLimit(.minutes(1)))
+	func interval() async throws {
+		let counter = Atomic<Int>(0)
+		let ticker = try Ticker()
+		try ticker.set(time: .init(value: -9000, timescale: 30000))
+		try ticker.set(rate: 1)
+		let timer = ticker.timer(for: .init(value: 1001, timescale: 30000))
+		for try await time in timer.values.prefix(20) {
+			#expect(CMTimeModApprox(time, .init(value: 1001, timescale: 30000)) == .zero)
+			counter.add(1, ordering: .acquiringAndReleasing)
+		}
+		#expect(counter.load(ordering: .acquiring) == 20)
+	}
+	@Test(TimeLimitTrait.timeLimit(.minutes(1)))
+	func offset() async throws {
 		let ticker = try Ticker()
 		let tocker = try Ticker(parent: ticker)
-		try ticker.set(rate: 1.5)
 		try tocker.set(rate: 1.2)
 		try tocker.set(time: .init(value: 300, timescale: 1), at: .init(seconds: 0, preferredTimescale: 1))
-		let cancel = tocker.vendor.sink(receiveValue: {
-			XCTAssertEqual($0.seconds, 420, accuracy: 0) // 300 + 100 * 1.2
-			expectation.fulfill()
-		})
+		try ticker.set(rate: 1.5)
 		try ticker.set(time: .init(seconds: 100, preferredTimescale: 1))
-		wait(for: [expectation], timeout: 0)
+		#expect((tocker.time.seconds - 420).magnitude < 1e-3)
 	}
 }
