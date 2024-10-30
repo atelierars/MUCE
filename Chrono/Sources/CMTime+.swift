0b89c74b01afe82f02809eca217cf77e35742835
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-30 09:51:31 +0400

    enrich times counter

--- Chrono/Sources/CMTime+.swift
+++ Chrono/Sources/CMTime+.swift
@@ -70,16 +70,35 @@ extension CMTime {
 extension CMTime {
 	@inlinable
 	@inline(__always)
-	public func times(of amount: CMTime) -> (count: Int, remainder: CMTime) {
-		assert([Int.bitWidth, CMTimeValue.bitWidth, CMTimeScale.bitWidth].allSatisfy{$0<=Int128.bitWidth})
+	public func times(of amount: CMTime, method: CMTimeRoundingMethod = .default) -> (count: Int, remainder: CMTime) {
 		if amount.isIndefinite {
 			return (0, self)
 		} else if isNumeric, amount.isNumeric {
+			assert([Int.bitWidth, CMTimeValue.bitWidth, CMTimeScale.bitWidth].allSatisfy{$0<=Int128.bitWidth})
 			let n = Int128(value) * Int128(amount.timescale)
 			let d = Int128(timescale) * Int128(amount.value)
 			let (q, r) = n.quotientAndRemainder(dividingBy: d)
-			let g = gcd(r, d)
-			return (Int(q), .init(value: .init(r/g), timescale: .init(d/g)))
+			switch method {
+			case.roundHalfAwayFromZero:
+				assertionFailure("approximation, use roundAwayFromZero")
+				fallthrough
+			case.roundAwayFromZero:
+				return (Int(r.signum() + q), .zero)
+			case.roundTowardNegativeInfinity:
+				return (Int(r.signum() < 0 ? q - 1 : q), .zero)
+			case.roundTowardPositiveInfinity:
+				return (Int(r.signum() > 0 ? q + 1 : q), .zero)
+			case.roundTowardZero:
+				return (Int(q), .zero)
+			case.quickTime:
+				assertionFailure("undefined, use another method")
+				fallthrough
+			case.default:
+				fallthrough
+			default:
+				let g = gcd(r, d)
+				return (Int(q), .init(value: .init(r/g), timescale: .init(d/g)))
+			}
 		} else {
 			return (0, .invalid)
 		}
