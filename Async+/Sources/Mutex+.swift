ffb782489a3659bfcae274aee629d05d891ce810
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-11-02 10:36:58 +0400

    update mutex

--- Async+/Sources/Mutex+.swift
+++ Async+/Sources/Mutex+.swift
@@ -7,7 +7,7 @@
 import struct Synchronization.Mutex
 extension Mutex where Value: Sendable {
 	@discardableResult
-	@inlinable
+	@inlinable @inline(__always)
 	public func replace(with value: Value) -> Value {
 		withLock { storage in
 			defer {
@@ -15,5 +15,14 @@ extension Mutex where Value: Sendable {
 			}
 			return storage
 		}
+	}
+}
+extension Mutex {
+	@discardableResult
+	@inlinable @inline(__always)
+	func take<Wrapped: Sendable>() -> Optional<Wrapped> where Value == Optional<Wrapped> {
+		withLockIfAvailable {
+			$0.take()
+		}.flatMap(\.self)
 	}
 }
